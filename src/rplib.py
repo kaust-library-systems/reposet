d1f713a2b00f510fd5ccca4581e25cca7ea27b16
Marcelo Garcia <marcelomgarcia@gmail.com>
2022-11-16 21:39:17 +0300

    Initial commit

---
+++ src/rplib.py
@@ -0,0 +1,18 @@
+# Functions for the reposet project
+
+import configparser as CP
+import logging as LOG
+
+def read_config(config_file: str) -> str:
+    ''' Read config file, and return diretory to read. '''
+
+    try:
+        config = CP.ConfigParser()
+        config._interpolation = CP.ExtendedInterpolation()
+        config.read(config_file)
+        files_dir = config['REPO']['IN_DIR']
+    except CP.Error as ee:
+        LOG.error(f"Error reading config file '{config_file}'")
+        files_dir = ""
+    
+    return files_dir
