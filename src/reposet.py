fde1ca398ff601c9e6a29b161131f4c3d9b93005
Marcelo Garcia <marcelomgarcia@gmail.com>
2022-11-16 21:39:59 +0300

    Read config file

--- src/reposet.py
+++ src/reposet.py
@@ -1,8 +1,22 @@
 # Write a CSV file with metadata of the repository items.
 # 
 
-def main():
-    pass
+import click as CL
+import rplib as RP
+import logging as LOG
+
+@CL.command()
+@CL.argument('input', type=CL.File('r'))
+def main(input):
+
+    LOG.basicConfig(encoding='utf-8', level=LOG.INFO)
+
+    files_dir = RP.read_config(input.name)
+    if not files_dir:
+        LOG.critical(f"The path of input files is empty")
+        raise Exception("Error getting the directory to read input files")
+
+    LOG.info(f"Input files in '{files_dir}'")
 
 if __name__ == "__main__":
     main()
