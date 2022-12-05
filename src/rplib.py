d0e27b468f5adad9483e7d7c7c670906537487b5
marcelo garcia <marcelomgarcia@gmail.com>
2022-12-05 20:07:16 +0300

    Format Update

--- src/rplib.py
+++ src/rplib.py
@@ -5,9 +5,10 @@ import logging as LOG
 import pathlib as PL
 import json as JSON
 
+
 def clean_json(json_file: str) -> dict:
-    '''Clean the JSON file by removing the external quotes.
-    Returns json data or an empty dict in case unable to clean. '''
+    """Clean the JSON file by removing the external quotes.
+    Returns json data or an empty dict in case unable to clean."""
 
     LOG.info(f"Trying to clean JSON file '{json_file}'")
     try:
@@ -16,21 +17,22 @@ def clean_json(json_file: str) -> dict:
         #
         text = str(in_text[0])
         len_text = len(text)
-        data = JSON.loads(text[1:len_text-1])
+        data = JSON.loads(text[1 : len_text - 1])
     except ValueError as ee:
         LOG.warning(f"Unable to clean JSON file '{json_file}'")
         data = {}
     finally:
         return data
 
+
 def read_json_file(json_file: str) -> dict:
-    '''Read the JSON file.
-    Returns the JSON data or an empty dict in case of error.'''
+    """Read the JSON file.
+    Returns the JSON data or an empty dict in case of error."""
 
     # Try to read the json
     LOG.info(f"Reading JSON file '{json_file}'")
     try:
-        with open(json_file, 'r') as fin:
+        with open(json_file, "r") as fin:
             data = JSON.load(fin)
     except ValueError as ee:
         LOG.error(f"Error {ee} while reading JSON file '{json_file}'.")
@@ -40,19 +42,22 @@ def read_json_file(json_file: str) -> dict:
 
 
 def get_metadata(json_file: str) -> dict:
-    '''Return a dictionary with article metadata from the json file'''
+    """Return a dictionary with article metadata from the json file"""
 
     data = read_json_file(json_file)
 
     if not data:
         return {}
     else:
-        entity_metadata = data['metadata']
-        row_csv = dict((ee['key'].split('.')[-1],ee['value']) for ee in entity_metadata)
+        entity_metadata = data["metadata"]
+        row_csv = dict(
+            (ee["key"].split(".")[-1], ee["value"]) for ee in entity_metadata
+        )
         return row_csv
 
+
 def get_json(root_dir: str) -> list:
-    '''Return  a list with the path to the JSON files'''
+    """Return  a list with the path to the JSON files"""
 
     files_path = PL.Path(root_dir)
     json_list = []
@@ -72,15 +77,15 @@ def get_json(root_dir: str) -> list:
 
 
 def read_config(config_file: str) -> str:
-    ''' Read config file, and return diretory to read. '''
+    """Read config file, and return diretory to read."""
 
     try:
         config = CP.ConfigParser(interpolation=CP.ExtendedInterpolation())
-        #config._interpolation = CP.ExtendedInterpolation()
+        # config._interpolation = CP.ExtendedInterpolation()
         config.read(config_file)
-        files_dir = config['REPO']['IN_DIR']
+        files_dir = config["REPO"]["IN_DIR"]
     except CP.Error as ee:
         LOG.critical(f"Error reading config file '{config_file}'")
         files_dir = ""
-    
+
     return files_dir
